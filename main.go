f8b539242de17f4f01485cc003fdb76ee83937b0
rendick <rendick123@proton.me>
2024-01-28 21:18:18 +0200

    new: text highlighging, os.Args[3] and switch{ case: }

--- main.go
+++ main.go
@@ -8,10 +8,22 @@ import (
 	"strconv"
 )
 
-var Answear int
+var (
+	// Colors
+	Green = "\033[32m"
+	Red   = "\033[31m"
+
+	Reset = "\033[0m"
+
+	// Text
+	Bold = "\033[1m"
+)
+
+var Answer int
+var Continue int
 
 func main() {
-	if len(os.Args) != 3 {
+	if len(os.Args) != 4 {
 		fmt.Println("Not enough arguments!")
 		os.Exit(0)
 	}
@@ -25,16 +37,38 @@ func main() {
 		if err != nil {
 			log.Fatal(err)
 		}
+
 		RandHundOne := rand.Intn(convert_first)
 		RandHundTwo := rand.Intn(convert_second)
-		fmt.Printf("Solve: %d + %d\n", RandHundOne, RandHundTwo)
-		fmt.Scan(&Answear)
+		fmt.Printf("Solve: %d %s %d\n", RandHundOne, os.Args[3], RandHundTwo)
+		fmt.Print("Your answer: ")
+		fmt.Scan(&Answer)
 
-		if Answear == (RandHundOne + RandHundTwo) {
-			fmt.Println("Good job!")
-			break
-		} else {
-			fmt.Println("Nope")
+		switch os.Args[3] {
+		case "+":
+			if Answer == (RandHundOne + RandHundTwo) {
+				fmt.Printf(Green + Bold + "\nCorrect!" + Reset + "\n------\n\n")
+			} else {
+				fmt.Printf(Red + Bold + "\nIncorrect! Try again." + Reset + "\n------\n\n")
+			}
+		case "-":
+			if Answer == (RandHundOne - RandHundTwo) {
+				fmt.Printf(Green + Bold + "\nCorrect!" + Reset + "\n------\n\n")
+			} else {
+				fmt.Printf(Red + Bold + "\nIncorrect! Try again." + Reset + "\n------\n\n")
+			}
+		case "*":
+			if Answer == (RandHundOne / RandHundTwo) {
+				fmt.Printf(Green + Bold + "\nCorrect!" + Reset + "\n------\n\n")
+			} else {
+				fmt.Printf(Red + Bold + "\nIncorrect! Try again." + Reset + "\n------\n\n")
+			}
+		case "/":
+			if Answer == (RandHundOne * RandHundTwo) {
+				fmt.Printf(Green + Bold + "\nCorrect!" + Reset + "\n------\n\n")
+			} else {
+				fmt.Printf(Red + Bold + "\nIncorrect! Try again." + Reset + "\n------\n\n")
+			}
 		}
 	}
 }
